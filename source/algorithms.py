99e9aea861ce82dd002491e91d078494ca2221d4
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2024-08-30 12:10:11 +0200

    epsilon greedy policy with small epsilon and episode_actions tracking

--- source/algorithms.py
+++ source/algorithms.py
@@ -83,6 +83,7 @@ class FarmAgentMCVFA:
         self.e_final = final_epsilon
         # self.poly = PolynomialFeatures(degree=2, include_bias=True)
         self.training_error = []
+        self.episode_actions = {}
         if initial_w is None:
             i_w = np.ones((self.env.action_space.n, self.env.unwrapped.features_number))
         else:
@@ -138,7 +139,7 @@ class FarmAgentMCVFA:
         e = []
         state, info = self.env.reset()
         for i in range(max_iterations):
-            action = self.greedy_policy(state)
+            action = self.policy(state)
             s_prime, reward, terminated, truncated, info = self.env.step(action=action)
             state_as_frozenset = frozenset(state.items()) # Convert the dictionary to a frozenset of items to avoid unashable type error
             e.append((state_as_frozenset, action, (self.gamma ** (i+1)) * reward))
@@ -148,12 +149,17 @@ class FarmAgentMCVFA:
                 state = s_prime
         return e
 
-    def update(self, max_iterations: int = 30):
+    def update(self,  episode_number: int, max_iterations: int = 30):
         """MC update rule"""
         # Generate an episode
         episode = self.generate_episode(max_iterations=max_iterations)
         visited = set()
         for i, (s, a, _) in enumerate(episode):
+            # Store the actions taken in each episode
+            if episode_number not in self.episode_actions.keys():
+                self.episode_actions[episode_number] = []
+            self.episode_actions[episode_number].append(a)
+            # Find G (substitute of the ground truth) if first visit of (s,a)
             if (s, a) not in visited:
                 visited.add((s, a))
                 G = sum(rw for _, _, rw in episode[i:])
